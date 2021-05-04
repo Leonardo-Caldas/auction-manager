4bd306f398dcc89d1bcdbd29d7733f43dfee0fb3
lolzeen <leonardo_nogueira13@hotmail.com>
2021-05-03 21:32:52 -0400

    third commit

---
+++ buyer_register.h
@@ -0,0 +1,15 @@
+#ifndef BUYER_REGISTER_H
+#define BUYER_REGISTER_H
+
+#include "register.h"
+namespace auction_manager
+{
+    class BuyerRegister : public Register
+    {
+        
+    };
+} //namespace auction_manager
+
+
+
+#endif // BUYER_REGISTER_H
